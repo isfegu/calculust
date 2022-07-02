5cadc3181cc83c3dff8ecca63a128ac797f1d868
isfegu <israelfenor@protonmail.com>
2022-07-02 12:32:11 +0200

    feat: habilita compilación como módulo WebAssembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,29 @@
 /// Performs an addition
-pub fn add(addend_a: f64, addend_b: f64) -> f64 {
+#[no_mangle]
+pub extern "C" fn add(addend_a: f64, addend_b: f64) -> f64 {
     addend_a + addend_b
 }
 
 /// Performs a subtraction
-pub fn sub(minuend: f64, substrahend: f64) -> f64 {
+#[no_mangle]
+pub extern "C" fn sub(minuend: f64, substrahend: f64) -> f64 {
     minuend - substrahend
 }
 
 /// Performs a multiplication
-pub fn mul(multiplicand: f64, multiplier: f64) -> f64 {
+#[no_mangle]
+pub extern "C" fn mul(multiplicand: f64, multiplier: f64) -> f64 {
     multiplicand * multiplier
 }
 
 /// Performs a division
-pub fn div(divisor: f64, dividend: f64) -> Result<f64, &'static str> {
+#[no_mangle]
+pub extern "C" fn div(divisor: f64, dividend: f64) -> f64 {
     if dividend == 0.0 {
-        return Err("Dividend is 0.");
+        return 0.0;
     }
 
-    Ok(divisor / dividend)
+    divisor / dividend
 }
 
 #[cfg(test)]
@@ -54,21 +58,21 @@ mod tests {
     #[test]
     fn div_works() {
         let result = super::div(2.0, 2.0);
-        assert_eq!(result.unwrap(), 1.0);
+        assert_eq!(result, 1.0);
 
         let result = super::div(-10.0, 2.0);
-        assert_eq!(result.unwrap(), -5.0);
+        assert_eq!(result, -5.0);
 
         let result = super::div(2.0, 10.0);
-        assert_eq!(result.unwrap(), 0.2);
+        assert_eq!(result, 0.2);
 
         let result = super::div(2.0, -10.0);
-        assert_eq!(result.unwrap(), -0.2);
+        assert_eq!(result, -0.2);
 
         let result = super::div(0.0, 2.0);
-        assert_eq!(result.unwrap(), 0.0);
+        assert_eq!(result, 0.0);
 
         let result = super::div(2.0, 0.0);
-        assert!(result.is_err());
+        assert_eq!(result, 0.0);
     }
 }
